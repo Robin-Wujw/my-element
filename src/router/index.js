26b12c7d8e79235aeb4ed15edc6a73818aa77c36
RobinWujw <robin.wujw@gmail.com>
2021-05-14 11:06:25 +0800

    后台管理系统

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import ListArticle from '../views/ListArticle'
+import CreateArticle from '../views/CreateArticle'
+import EditArticle from '../views/EditArticle'
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    redirect: '/articles/index'
+  },
+  {
+    path: '/articles/create',
+    name: 'create-article',
+    component: CreateArticle
+  },
+  {
+    path: '/articles/index',
+    name: 'List-article',
+    component: ListArticle
+  },
+  {
+    path: '/articles/:id/edit',
+    name: 'edit-article',
+    component: EditArticle
+  },
+]
+
+const router = new VueRouter({
+  routes
+})
+
+export default router
